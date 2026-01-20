187d479196684df860dc2e7edab6387ad5e7ef4c
ricardodeazambuja <ricardo.azambuja@gmail.com>
2026-01-19 19:02:15 -0500

    Updates

--- src/utils/constants.ts
+++
@@ -1,9 +0,0 @@
-export const RECTCOLOURS: Record<string, string> = {
-    'r': 'red',
-    'g': 'green',
-    'b': 'blue',
-    'y': 'yellow',
-    'o': 'orange',
-};
-
-export const RECTCOLOURS_KEYS = Object.keys(RECTCOLOURS);
